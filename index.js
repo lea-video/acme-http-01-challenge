c8c6028aa35b3f873ff980177b2918abf2e985ef
TimeForANinja <t.kutscha@yahoo.de>
2020-07-22 01:55:21 +0200

    setup linter & lint

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
+/* eslint-disable no-console */
 const storage = new Map();
 
-// based on https://git.coolaj86.com/coolaj86/acme-http-01-standalone.js
-module.exports.create = function(config) {
-	let verbose = config.verbose || false;
+// Based on https://git.coolaj86.com/coolaj86/acme-http-01-standalone.js
+module.exports.create = function create(config) {
+  let verbose = config.verbose || false;
 
-	return {
-		init: function(opts) {
-			if (verbose) console.log('Init Key Auth URL', opts, storage);
-			return Promise.resolve(null);
-		},
+  return {
+    init: function init(opts) {
+      if (verbose) console.log('Init Key Auth URL', opts, storage);
+      return Promise.resolve(null);
+    },
 
-		set: function(data) {
-			if (verbose) console.log('Add Key Auth URL', data, storage);
+    set: function set(data) {
+      if (verbose) console.log('Add Key Auth URL', data, storage);
 
-			const ch = data.challenge;
-			const key = ch.identifier.value + '#' + ch.token;
-			storage.set(key, ch);
+      const ch = data.challenge;
+      const key = `${ch.identifier.value}#${ch.token}`;
+      storage.set(key, ch);
 
-			return Promise.resolve(null);
-		},
+      return Promise.resolve(null);
+    },
 
-		get: function(data) {
-			if (verbose) console.log('List Key Auth URL', data, storage);
+    get: function get(data) {
+      if (verbose) console.log('List Key Auth URL', data, storage);
 
-			const ch = data.challenge;
-			const key = ch.identifier.value + '#' + ch.token;
-			if (storage.has(key)) return Promise.resolve(storage.get(key));
+      const ch = data.challenge;
+      const key = `${ch.identifier.value}#${ch.token}`;
+      if (storage.has(key)) return Promise.resolve(storage.get(key));
 
-			return Promise.resolve(null);
-		},
+      return Promise.resolve(null);
+    },
 
-		remove: function(data) {
-			if (verbose) console.log('Remove Key Auth URL', data, storage);
+    remove: function remove(data) {
+      if (verbose) console.log('Remove Key Auth URL', data, storage);
 
-			const ch = data.challenge;
-			const key = ch.identifier.value + '#' + ch.token;
-			storage.delete(key);
+      const ch = data.challenge;
+      const key = `${ch.identifier.value}#${ch.token}`;
+      storage.delete(key);
 
-			return Promise.resolve(null);
-		}
-	};
+      return Promise.resolve(null);
+    },
+  };
 };

c8594295a5b380a82102ef49d850bf13dc957e09
TimeForANinja <t.kutscha@yahoo.de>
2020-07-22 01:50:20 +0200

    implement storage

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-module.exports.create = function(config) {
-	console.log('New http-challenge-client', config);
+const storage = new Map();
 
-	let storage = [];
-	/** stores data instances:
-		data.challenge.token
-		data.challenge.keyAuthorization
-	*/
+// based on https://git.coolaj86.com/coolaj86/acme-http-01-standalone.js
+module.exports.create = function(config) {
+	let verbose = config.verbose || false;
 
 	return {
 		init: function(opts) {
-			console.log('Init Key Auth URL', opts);
-			return null;
+			if (verbose) console.log('Init Key Auth URL', opts, storage);
+			return Promise.resolve(null);
 		},
 
 		set: function(data) {
-			console.log('Add Key Auth URL', data);
+			if (verbose) console.log('Add Key Auth URL', data, storage);
 
-			storage.push(data);
+			const ch = data.challenge;
+			const key = ch.identifier.value + '#' + ch.token;
+			storage.set(key, ch);
+
+			return Promise.resolve(null);
 		},
 
 		get: function(data) {
-			console.log('List Key Auth URL', data);
+			if (verbose) console.log('List Key Auth URL', data, storage);
 
-			for (const s of storage) {
-				if (s.challenge.token === data.challenge.token) return s.challenge.keyAuthorization;
-			}
-			return null;
+			const ch = data.challenge;
+			const key = ch.identifier.value + '#' + ch.token;
+			if (storage.has(key)) return Promise.resolve(storage.get(key));
+
+			return Promise.resolve(null);
 		},
 
 		remove: function(data) {
-			console.log('Remove Key Auth URL', data);
+			if (verbose) console.log('Remove Key Auth URL', data, storage);
 
-			storage = storage.filter(s => {
-				return s.challenge.token === data.challenge.token;
-			});
+			const ch = data.challenge;
+			const key = ch.identifier.value + '#' + ch.token;
+			storage.delete(key);
+
+			return Promise.resolve(null);
 		}
 	};
 };
